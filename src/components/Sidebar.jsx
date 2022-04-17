3c290e7b36a56cf4945987a5976012da179cf513
novianto22 <99942803+novianto22@users.noreply.github.com>
2022-04-17 22:06:40 +0700

    Add button add categories

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,12 +7,31 @@ const DATA = [
 ];
 const Sidebar = () => {
   return (
-    <div className="w-full">
-      <div className="mt-20 px-8">
+    <div className="w-full h-full relative">
+      <div className="pt-20 px-8">
         <h2 className="text-2xl font-semibold mb-6">Categories</h2>
         {DATA.map((expense) => (
           <CategoriesItem key={expense.id} expense={expense} />
         ))}
+      </div>
+      <div className="px-8 absolute bottom-8">
+        <button className="text-lg font-semibold text-blue-600">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 inline align-text-top mr-2"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 4v16m8-8H4"
+            />
+          </svg>
+          Add Categories
+        </button>
       </div>
     </div>
   );
